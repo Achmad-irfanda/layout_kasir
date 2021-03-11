ceb22a6a595d89142bffa5bbfc48f94402102768
Achmad-irfanda <achmadirfanda199@gmail.com>
2021-03-12 06:31:35 +0700

    edit halaman home

---
+++ lib/home/home.dart
@@ -0,0 +1,303 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:layout_kasir/home/cardmenuset.dart';
+
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  String nama;
+  String harga;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            //  RaisedButton.icon(onPressed: null, icon:  , label: null)
+          ],
+          title: Text('Kasir'),
+        ),
+        body: Stack(
+          children: [
+            ListView(
+              children: [
+                // searchbar
+                new Container(
+                    padding: EdgeInsets.only(top: 10, left: 20, bottom: 10),
+                    margin: EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        Icon(Icons.search),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child:
+                              Text('Cari Menu', style: TextStyle(fontSize: 18)),
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1),
+                        borderRadius: BorderRadius.circular(10))),
+
+                // kolom untuk menampung bagian kartegori
+                new Container(
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Text(
+                            'KATEGORI',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ),
+
+                      // row untuk menampung item kasir
+                      Row(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(
+                                  left: 20, right: 15, top: 20, bottom: 20),
+                              padding: EdgeInsets.only(
+                                  left: 10, right: 10, top: 5, bottom: 5),
+                              child: Text(
+                                'Semua',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.purple[900],
+                                    width: 2,
+                                  ),
+                                  borderRadius: BorderRadius.circular(30))),
+                          Container(
+                              margin: EdgeInsets.only(
+                                  right: 15, top: 20, bottom: 20),
+                              padding: EdgeInsets.only(
+                                  left: 10, right: 10, top: 5, bottom: 5),
+                              child: Text(
+                                'Makanan',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                    width: 2,
+                                  ),
+                                  borderRadius: BorderRadius.circular(30))),
+                          Container(
+                              margin: EdgeInsets.only(
+                                  right: 15, top: 20, bottom: 20),
+                              padding: EdgeInsets.only(
+                                  left: 10, right: 10, top: 5, bottom: 5),
+                              child: Text(
+                                'Minuman',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                    width: 2,
+                                  ),
+                                  borderRadius: BorderRadius.circular(30))),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+            // item kasir 
+            new Container(
+                  margin: EdgeInsets.only(left: 10, right: 10, bottom: 600),
+                  child: Column(
+                    children: [
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        crossAxisCount: 2,
+                        childAspectRatio: 6 / 3,
+                        children: [
+                          card(
+                              nama: "Sambel Matah Chicken Ricebowl ",
+                              harga: "20.00"),
+                          card(nama: "Chicken Ricebowl", harga: "20.00")
+                        ],
+                      ),
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        crossAxisCount: 2,
+                        childAspectRatio: 6 / 3,
+                        children: [
+                          card(nama: "Tuna Ricebowl", harga: "20.000"),
+                          card(nama: "Nasi Goreng Spesial", harga: "20.00")
+                        ],
+                      ),
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        crossAxisCount: 2,
+                        childAspectRatio: 6 / 3,
+                        children: [
+                          card(nama: "Espresso", harga: "15.000"),
+                          card(nama: "Caffe Latte", harga: "20.00")
+                        ],
+                      ),
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        crossAxisCount: 2,
+                        childAspectRatio: 6 / 3,
+                        children: [
+                          card(nama: "Hot Ice Lemon Tea", harga: "8.000"),
+                          card(nama: "Ice Butterfly Coffe", harga: "20.00")
+                        ],
+                      ),
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        crossAxisCount: 2,
+                        childAspectRatio: 6 / 3,
+                        children: [
+                          card(nama: "Hot Ice Lemon Tea", harga: "8.000"),
+                          card(nama: "Ice Butterfly Coffe", harga: "20.00")
+                        ],
+                      ),
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        crossAxisCount: 2,
+                        childAspectRatio: 6 / 3,
+                        children: [
+                          card(nama: "Hot Ice Lemon Tea", harga: "8.000"),
+                          card(nama: "Ice Butterfly Coffe", harga: "20.00")
+                        ],
+                      ),
+                      GridView.count(
+                        shrinkWrap: true,
+                        crossAxisCount: 2,
+                        childAspectRatio: 6 / 3,
+                        children: [
+                          card(nama: "Hot Ice Lemon Tea", harga: "8.000"),
+                          card(nama: "Ice Butterfly Coffe", harga: "20.00")
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+
+
+            DraggableScrollableSheet(
+              initialChildSize: 0.5,
+              minChildSize: 0.1,
+              maxChildSize: 0.6,
+              builder:
+                  (BuildContext context, ScrollController scrollController) {
+                return Container(
+                    color: Colors.white,
+                    child: ListView(
+                      controller: scrollController,
+                      children: [
+                        Image.asset(
+                          "assets/tip.png",
+                          height: 10.0,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 20, right: 20),
+                          child: Text(
+                            "TOTAL",
+                            style: TextStyle(
+                                fontSize: 19,
+                                color: Colors.purple[900],
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                              left: 20, right: 20, top: 6, bottom: 20),
+                          child: Text(
+                            "RP. 27.0000 (ini hanya contoh)",
+                            style: TextStyle(
+                                fontSize: 22,
+                                color: Colors.purple[900],
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              top: BorderSide(width: 1.0, color: Colors.grey),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                          child: InputDecorator(
+                            child: Text("totalnya ada disini"),
+                            decoration: InputDecoration(
+                              labelText: 'Subtotal',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                          child: InputDecorator(
+                            decoration: InputDecoration(
+                              labelText: 'Jumlah Dibayarkan',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "masukkan nominal pembayaran"),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                          child: InputDecorator(
+                            decoration: InputDecoration(
+                              labelText: 'Jumlah Kembalian',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            child: Text('Kembaliannya ada disini'),
+                          ),
+                        ),
+                        new Container(
+                          margin: EdgeInsets.only(
+                              left: 40, right: 40, top: 30, bottom: 50),
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            color: Colors.purple[900],
+                            onPressed: () => print("Button Pressed"),
+                            child: new Text(
+                              "Proses Pembayaran",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                        )
+                      ],
+                    ));
+              },
+            ),
+          ],
+        ));
+  }
+}
